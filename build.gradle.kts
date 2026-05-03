b7bde7cc35c9f892ff7c34beb58efa3aae48e7e6
Hamza Qazi <night.dev@hotmail.com>
2026-05-03 17:39:46 -0500

    build: shade JDA, sqlite-jdbc, night-config into mod jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("fabric-loom") version "1.7-SNAPSHOT"
     id("java")
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 val modId: String by project
@@ -25,11 +26,21 @@ repositories {
     maven("https://maven.fabricmc.net/")
 }
 
+val shade: Configuration by configurations.creating
+configurations.implementation.get().extendsFrom(shade)
+
 dependencies {
     minecraft("com.mojang:minecraft:$minecraftVersion")
     mappings("net.fabricmc:yarn:$yarnMappings:v2")
     modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
     modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricVersion")
+
+    shade("net.dv8tion:JDA:5.2.1") {
+        exclude(module = "opus-java")
+    }
+    shade("org.xerial:sqlite-jdbc:3.46.1.3")
+    shade("com.electronwill.night-config:toml:3.8.1")
+    shade("org.slf4j:slf4j-api:2.0.16")
 }
 
 tasks.processResources {
@@ -38,6 +49,23 @@ tasks.processResources {
     filesMatching("fabric.mod.json") {
         expand(mapOf("version" to project.version, "mod_id" to modId))
     }
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("dev-shadow")
+    configurations = listOf(shade)
+    relocate("net.dv8tion.jda", "com.westwardmc.dmcl.shaded.jda")
+    relocate("com.electronwill.nightconfig", "com.westwardmc.dmcl.shaded.nightconfig")
+    relocate("org.sqlite", "com.westwardmc.dmcl.shaded.sqlite")
+    mergeServiceFiles()
+    minimize {
+        exclude(dependency("org.xerial:sqlite-jdbc:.*"))
+    }
+}
+
+tasks.remapJar {
+    dependsOn(tasks.shadowJar)
+    inputFile.set(tasks.shadowJar.flatMap { it.archiveFile })
 }
 
 tasks.withType<JavaCompile>().configureEach {
